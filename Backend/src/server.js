e63704c432a215f7ab20326d5935b4db4af9e633
GuptaShubham1512 <shubhamkrgupta15@gmail.com>
2026-09-08 16:01:30 +0530

    change 6

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -13,6 +13,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://shubham-ai-portfolio.vercel.app",
+      "https://shubham-ai-portfolio-backend.onrender.com"
     ],
   }),
 );
